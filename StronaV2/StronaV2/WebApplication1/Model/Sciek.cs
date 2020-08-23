8d5e3604c2a8f6cbeb76c21fd023648cd4e77ce2
reqqus <t_losek@poczta.wwsi.edu.pl>
2020-08-23 20:01:12 +0200

    Pokazwyanie scieka

---
+++ StronaV2/StronaV2/WebApplication1/Model/Sciek.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Model
+{
+    public class Sciek
+    {
+        [Key]
+        public int id_klienta { get; set; }
+        [Required]
+        public string Imie { get; set; }
+        public string Nazwisko { get; set; }
+        public char CzyPelnoletni { get; set; }
+        public int Ulub_alkohol { get; set; }
+        public int Ulub_drink { get; set; }
+
+    }
+}
